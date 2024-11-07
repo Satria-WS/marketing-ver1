c936d5e49150bdd41563ec7b89f232099cbf67a4
Satria-WS <extencezone02@gmail.com>
2024-11-07 13:40:41 +0700

    Update Hero.jsx with new element

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,26 +6,83 @@ const Hero = () => {
     <>
       <div className="container flex flex-col md:flex-row border border-orange-300">
         {/* Text */}
-        <div className="bg-slate-500 basis-1/2  ">
-          {/* <motion.h3 className="text-2xl">
-          Unlock a Passion, Side Hustle, or New
-        </motion.h3>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos
-          eligendi mollitia
-        </p>
-        <button className="bg-black">
-          <p className="text-white text-sm font-semibold py-2 px-4"> More News</p>
-        </button> */}
-          x
+        <div className="flex flex-col items-center justify-center basis-1/2 space-y-5 text-center min-h-[300px]  py-20 px-10 ">
+          <motion.h1
+            // sliding Up
+            variants={{
+              initial: {
+                y: 50,
+                opacity: 0,
+              },
+              animate: {
+                y: 0,
+                opacity: 1,
+                transition: {
+                  duration: 0.5,
+                  delay: 0.2,
+                },
+              },
+            }}
+            initial="initial"
+            animate="animate"
+            className="text-4xl font-bold"
+          >
+            Unlock a Passion, Side Hustle, or New
+            <span className="text-gray-400 underline">Profession</span>
+          </motion.h1>
+          <motion.p
+            variants={{
+              initial: {
+                y: 50,
+                opacity: 0,
+              },
+              animate: {
+                y: 0,
+                opacity: 1,
+                transition: {
+                  duration: 0.5,
+                  delay: 0.6,
+                },
+              },
+            }}
+            initial="initial"
+            animate="animate"
+          >
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Dignissimos eligendi mollitia
+          </motion.p>
+          <motion.button
+            variants={{
+              initial: {
+                y: 50,
+                opacity: 0,
+              },
+              animate: {
+                y: 0,
+                opacity: 1,
+                transition: {
+                  duration: 0.5,
+                  delay: 0.8,
+                },
+              },
+            }}
+            initial="initial"
+            animate="animate"
+            className="bg-black hover:bg-primary duration-300 opacity-100"
+          >
+            <p className="text-white text-sm font-semibold py-2 px-4">
+              More News
+            </p>
+          </motion.button>
         </div>
         {/* Picture */}
-        <div className="bg-orange-300 basis-1/2 ">
-          {/* <img src={logo} alt="" /> */}
-          <p>x</p>
+        <div className=" basis-1/2 flex justify-center items-center ">
+          <img className="w-full max-w-[550px] " src={logo} alt="" />
         </div>
       </div>
     </>
   );
 };
 export default Hero;
+
+// w-[90%] md:max-w-[550px] xl:max-w-[600px]
