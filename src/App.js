b17ab0d268833d3eda0548c7f5127b09b74e0b83
Satria-WS <extencezone02@gmail.com>
2024-11-07 13:38:46 +0700

    Organize and add components structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import Banner from "./components/Banner";
+import Card from "./components/Card";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Section from "./components/Section";
+import Service from "./components/Service";
 
 function App() {
   return (
     <div className="p-5">
       <main>
         <Navbar />
-        <Hero/>
+        <Hero />
+        <Service />
+        <Banner />
+        <Card />
+        <Section/>
      </main>
     </div>
   );
