55a50cfa95439ce08643efebfd879ebe2c782d9c
Satria-WS <extencezone02@gmail.com>
2024-11-07 13:41:44 +0700

    Add section element to jsx comeponent

---
+++ src/components/Section.jsx
@@ -0,0 +1,30 @@
+import image_4 from "../assets/image/4.png"
+
+const Section = () => {
+  return (
+    <div>
+      <div>
+        <img src={image_4} alt="orang-ngelamun"/>
+      </div>
+      <div>
+        <p>Tech Addict</p>
+        <h1>Tech really Understood Our Needs</h1>
+        <button>More News</button>
+      </div>
+      <div>
+        <h1>Join our email</h1>
+        <p>Get 20% off per order</p>
+        <button>Subcribe</button>
+      </div>
+      <div>
+        <ul>
+          <li>Fb</li>
+          <li>Twitter</li>
+          <li>Instagram</li>
+          <li>Linkin</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+export default Section
