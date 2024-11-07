20d1a4ca7c3e1852064ac3baaac33bbc4884a390
Satria-WS <extencezone02@gmail.com>
2024-11-07 13:41:26 +0700

    add structure element Card.jsx

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+const Card = () => {
+  return (
+    <>
+      <div className="border border-purple-300 p-1 bg-gray-50">
+        <div className="container border border-purple-300">
+          <div>Card 1</div>
+          <div>Card 2</div>
+          <div>Card 3</div>
+          <div>Card 4</div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Card;
