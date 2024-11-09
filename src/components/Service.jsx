2188458f8f59815f56ddadb7e2812bced24e823f
Satria-WS <extencezone02@gmail.com>
2024-11-09 17:39:57 +0700

    css fixed

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -7,18 +7,18 @@ const Service = () => {
   return (
     <>
       <div className="container border border-pink-400 ">
-        <h1 className="text-4xl font-bold py-12">
+        <h1 className="text-4xl font-bold py-12 xl:max-w-[300px]">
           Classes to Spark Your
-          <span className="text-gray-400 underline">Creativity</span>
+          <span className="text-gray-400 underline pl-5">Creativity</span>
         </h1>
         {/* card */}
-        <div className="grid grid-cols-1 grid-rows-1 gap-1 ">
+        <div className="grid grid-cols-1 grid-rows-1 md:grid-cols-2 gap-1 ">
           {/* image */}
           <motion.div
             initial={{ opacity: 0, x: 100 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.5, delay: 0.2 }}
-            className="flex justify-center items-center p-5 bg-red-500 "
+            className="flex justify-center items-center p-5 "
           >
             <img className="w-full max-w-[300px]" src={logoPlant} alt="plant" />
           </motion.div>
